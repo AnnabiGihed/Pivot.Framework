4e0b10dfaf6a0b475a74fb6b460d33a632beb226
Gihed Annabi <annabi.gihed@gmail.com>
2025-01-09 23:58:15 +0100

    test

--- Src/Infrastructure/Templates.Core.Infrastructure.Persistence.EntityFrameworkCore/Outbox/Publisher/DomainEventPublisher.cs
+++ Src/Infrastructure/Templates.Core.Infrastructure.Persistence.EntityFrameworkCore/Outbox/Publisher/DomainEventPublisher.cs
@@ -5,16 +5,19 @@ using Templates.Core.Domain.Primitives;
 using Templates.Core.Infrastructure.Abstraction.Outbox.Models;
 using Templates.Core.Infrastructure.Abstraction.Outbox.Repositories;
 using Templates.Core.Infrastructure.Abstraction.Outbox.DomainEventPublisher;
+using Microsoft.Extensions.Logging;
 
 namespace Templates.Core.Infrastructure.Persistence.EntityFrameworkCore.Outbox.Publisher;
 
 public class DomainEventPublisher<TContext> : IDomainEventPublisher where TContext : DbContext
 {
+	protected readonly ILogger<DomainEventPublisher<TContext>> _logger;
 	protected readonly IOutboxRepository<TContext> _outboxRepository;
 
-	public DomainEventPublisher(IOutboxRepository<TContext> outboxRepository)
+	public DomainEventPublisher(IOutboxRepository<TContext> outboxRepository, ILogger<DomainEventPublisher<TContext>> logger)
 	{
 		_outboxRepository = outboxRepository;
+		_logger = logger;
 	}
 
 	public async Task PublishAsync(IDomainEvent domainEvent, CancellationToken cancellationToken)
@@ -37,6 +40,9 @@ public class DomainEventPublisher<TContext> : IDomainEventPublisher where TConte
 				Payload = serializedObject,
 				CreatedAtUtc = domainEvent.OccurredOnUtc
 			};
+
+			_logger.LogWarning($"Publishing domain event Type: {domainEvent?.GetType()?.AssemblyQualifiedName}");
+
 
 			await _outboxRepository.AddAsync(outboxMessage, cancellationToken);
 		}
